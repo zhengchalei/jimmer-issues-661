4c9f0b94942855c33e0ebb5d459aa4e524d7af55
zhengchalei <stone981023@gmail.com>
2024-09-07 11:46:27 +0800

    Initial commit

---
+++ src/test/kotlin/com/zhengchalei/jimmerdemo/JimmerDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zhengchalei.jimmerdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JimmerDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
